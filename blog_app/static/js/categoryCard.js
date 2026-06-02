eff1d60f1933b28082a29677ad8f7f0a83d3a479
Linar-S <test@test.ru>
2026-06-02 17:13:24 +0300

    Добавили форму заполнения категорий

---
+++ blog_app/static/js/categoryCard.js
@@ -0,0 +1,31 @@
+function setIcon(){
+    const iconValue = document.getElementsByClassName('category-icon')[0].value;
+    const iconElem = document.getElementById("icon");
+    iconElem.className = `fa ${iconValue}`
+}
+
+function addIcon(){
+const iconInput = document.getElementsByClassName('category-icon')[0];
+const iconPElem = iconInput.parentNode.children[0];
+const iconElem = document.createElement("i");
+
+iconPElem.classList.add("flex");
+iconPElem.classList.add("flex-jc-sb");
+
+iconInput.addEventListener("input", setIcon);
+
+iconElem.id = "icon";
+iconElem.className = "fa";
+
+if (iconInput.value) {
+    iconElem.className = `fa ${iconInput.value}`
+}
+
+iconPElem.appendChild(iconElem);
+
+
+console.log(iconPElem);
+}
+
+document.addEventListener("DOMContentLoaded", addIcon);
+
